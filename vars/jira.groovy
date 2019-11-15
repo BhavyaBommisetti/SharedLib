5827409545a509458363c7ba5fa7588007285633
maheedhar132 <45238528+maheedhar132@users.noreply.github.com>
2019-11-15 15:30:51 +0530

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,4 +1,12 @@
 def call(body){
+  DefaultHttpClient client = new DefaultHttpClient();
+			
+HttpGet getRequest = new HttpGet(url + "rest/auth/latest/session");
+getRequest.addHeader("Authorization", "Basic " + userToken);
+getRequest.addHeader("accept", "application/json");
+HttpResponse getResponse = client.execute(getRequest);
+
+HttpPost postRequest = new HttpPost(....);
 def issue = [fields: [ project: [key: 'PRJ'],
                        summary: 'New JIRA Created from Jenkins.',
                        description: 'New JIRA Created from Jenkins.',
