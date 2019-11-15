fb1582c7c7dbe3155eaab3352fb6c34399a6e7e2
maheedhar132 <45238528+maheedhar132@users.noreply.github.com>
2019-11-15 14:50:31 +0530

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,4 +1,8 @@
 def call(body){
- def request = libraryResource 'data.json'
- jiraNewIssue(request)
+def issue = [fields: [ project: [key: 'PRJ'],
+                       summary: 'New JIRA Created from Jenkins.',
+                       description: 'New JIRA Created from Jenkins.',
+                       issuetype: [name: 'Bug']]]
+def newIssue = jiraNewIssue issue: issue, site: 'https://jira1320.atlassian.net'
+echo newIssue.data.key
 }
