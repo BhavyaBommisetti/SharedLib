6f5551e652cc20443f8f699eac2894210f784586
maheedhar132 <45238528+maheedhar132@users.noreply.github.com>
2019-11-15 15:38:57 +0530

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,8 +1,8 @@
 def call(body){
 
-def issue = [fields: [ project: [key: 'PRJ'],
-                       summary: 'New JIRA Created from Jenkins.',
-                       description: 'New JIRA Created from Jenkins.',
+def issue = [fields: [ project: [key: 'PRO'],
+                       summary: 'BUG created',
+                       description: 'jira bug',
                        issuetype: [name: 'Bug']]]
 def newIssue = jiraNewIssue issue: issue, site: 'jira'
 echo newIssue.data.key
